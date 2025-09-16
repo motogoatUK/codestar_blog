c4664e7b12b6d6fb3ba620eb4c71dda29205b95d
motogoatUK <software@motogoat.co.uk>
2025-09-16 17:36:22 +0100

    add excerpt field and modified_on fields

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
     )
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
