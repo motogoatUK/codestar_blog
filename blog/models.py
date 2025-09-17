b480e38995ac396e969cc8ff792b419c2e2c0363
motogoatUK <software@motogoat.co.uk>
2025-09-17 21:54:47 +0100

    add Meta and __str__ method to Posts

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
