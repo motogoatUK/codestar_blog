a429fffd029a3728571527282c70fb3e55f21381
motogoatUK <software@motogoat.co.uk>
2025-09-17 21:30:09 +0100

    add Comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
-                 '127.0.0.1',
+                 '127.0.0.1'
                  ]
 
 CSRF_TRUSTED_ORIGINS = [
