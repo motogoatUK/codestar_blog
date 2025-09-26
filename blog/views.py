8cad4aef5602eb7ec1c80bc8c6555b70a581f77b
motogoatUK <software@motogoat.co.uk>
2025-09-26 14:22:35 +0100

    install crispy and add comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -29,6 +30,16 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+    comment_form = CommentForm
+
     return render(
         request,
         "blog/post_detail.html",
@@ -36,5 +47,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
