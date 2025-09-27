33b0948be0d44e756dab9789e267b5b09ab35ea5
motogoatUK <software@motogoat.co.uk>
2025-09-27 21:30:01 +0100

    add uploaded images to about and post_detail

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -6,6 +7,10 @@ class About(models.Model):
     title = models.CharField(max_length=50, unique=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    profile_image = CloudinaryField(
+        'image',
+        default='placeholder'
+    )
 
     def __str__(self):
         return self.title
