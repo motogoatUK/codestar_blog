531a5946c942c75630d133bb50aa7bec4aa39ab4
motogoatUK <software@motogoat.co.uk>
2025-09-27 12:32:40 +0100

    add POST handling for collab form

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -7,6 +8,15 @@ from .forms import CollaborateForm
 def about_me(request):
     about = About.objects.order_by("-updated_on").first()
     # about = get_object_or_404(queryset)
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save(commit=False)
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! \
+                 I endeavour to respond within 2 working days."
+            )
 
     collaborate_form = CollaborateForm
 
